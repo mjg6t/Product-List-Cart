dfda6f958425a988935cf70cc88b255a2f2e8b76
MJG <ostalot@proton.me>
2024-11-06 21:32:08 +0500

    added more functionality and improved how it looks on phones

--- main.js
+++ main.js
@@ -5,9 +5,10 @@ const cart = document.getElementById("inside-cart")
 const dessertList = document.getElementById("list-items")
 console.log(dessertList)
 
+ let shoppingCart = []
+ let totalItems = sumShoppingCart(shoppingCart)
 
-
-if (cartSizeValue == 0) {
+if (totalItems == 0) {
     let newDiv = document.createElement('div');
 
     let newImg = document.createElement('img');
@@ -64,6 +65,8 @@ function listDesserts(dessert){
     button.appendChild(icon)
     button.appendChild(document.createTextNode('add to cart'))
 
+    
+
     item.appendChild(img)
     item.appendChild(button)
     item.appendChild(category)
@@ -71,6 +74,39 @@ function listDesserts(dessert){
     item.appendChild(price)
 
     item.classList.add('item')
+
+    const addicon = document.createElement('img')
+    addicon.src = 'assets/images/icon-increment-quantity.svg'
+    addicon.width = 10
+    addicon.height = 10
+
+    const subicon = document.createElement('img')
+    subicon.src = 'assets/images/icon-decrement-quantity.svg'
+    subicon.width = 10
+    subicon.height = 10
+
+
+    button.addEventListener("click",()=>{
+        img.classList.add('on-selected-or-hover')
+        button.classList.add('button-hover')
+        button.textContent = ''
+        button.appendChild(subicon)
+        button.appendChild(document.createTextNode(`${returnQuantity(dessert.name)}`))
+        button.appendChild(addicon)
+
+    })
+
+    addicon.addEventListener("click",()=>{
+        addQuantity(dessert.name)
+        totalItems = sumShoppingCart(shoppingCart)
+        cartSize.innerText=`(${totalItems})`
+    })
+
+    subicon.addEventListener("click",()=>{
+        subQuantity(dessert.name)
+        totalItems = sumShoppingCart(shoppingCart)
+        cartSize.innerText=`(${totalItems})`
+    })
 
     dessertList.appendChild(item)
     
@@ -83,9 +119,54 @@ async function getDesserts(){
     let desserts = []
     const res = await fetch('data.json')
     desserts= await res.json()
+    desserts.forEach((dessert)=>populateCart(dessert))
     desserts.forEach((dessert)=>listDesserts(dessert))
 
 }
 
+function populateCart(dessert){
+    const des ={
+        item: dessert.name,
+        quantity: 0
+    }
+    shoppingCart.push(des)
+}
+
+function returnQuantity(name){
+    let item = shoppingCart.find((item)=>{
+        return item.item == name
+    })
+    return item.quantity
+}
+
+function addQuantity(name){
+    let item = shoppingCart.find((item)=>{
+        return item.item == name
+    })
+    
+    item.quantity = item.quantity+1
+    return item.quantity
+}
+
+function subQuantity(name){
+    let item = shoppingCart.find((item)=>{
+        return item.item == name
+    })
+
+    if (item.quantity == 0){
+        return item.quantity
+    }
+    
+    item.quantity = item.quantity-1
+    return item.quantity
+}
+
+function sumShoppingCart(shoppingCart){
+    let total = 0;
+    shoppingCart.forEach((item) => {
+        total += item.quantity;
+    });
+    return total;
+}
 
 
