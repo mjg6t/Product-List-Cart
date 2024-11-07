ac4d410428cafa512db4e800d4c9ec3fc39ced29
MJG <ostalot@proton.me>
2024-11-07 23:24:21 +0500

    started working on the cart + changed margins on mobile

--- main.js
+++ main.js
@@ -4,24 +4,79 @@ cartSizeValue = cartSizeValue.replace(/[\(\)]/g, '')
 const cart = document.getElementById("inside-cart")
 const dessertList = document.getElementById("list-items")
 
- let shoppingCart = []
- let totalItems = sumShoppingCart(shoppingCart)
+let shoppingCart = []
+let totalItems = sumShoppingCart(shoppingCart)
+let cartEmpty = true
+let hasEmptyCartRunBefore = false
 
-if (totalItems == 0) {
-    let newDiv = document.createElement('div');
+emptyCart()
 
-    let newImg = document.createElement('img');
-    newImg.src = 'assets/images/illustration-empty-cart.svg';
-    // newImg.width = 250;
-    // newImg.height = 150;
-    newDiv.appendChild(newImg);
+function emptyCart(){
 
-     let newText = document.createElement('div');
-     newText.innerText = 'your added items will appear here'
-     newDiv.appendChild(newText);
+    if (cartEmpty==true && hasEmptyCartRunBefore == false){
 
-    newDiv.classList.add('empty-cart')
-    cart.appendChild(newDiv)
+        let newDiv = document.createElement('div');
+        newDiv.id = 'newDiv'
+
+        let newImg = document.createElement('img');
+        newImg.src = 'assets/images/illustration-empty-cart.svg';
+        // newImg.width = 250;
+        // newImg.height = 150;
+        newDiv.appendChild(newImg);
+
+        let newText = document.createElement('div');
+        newText.innerText = 'your added items will appear here'
+        newDiv.appendChild(newText);
+
+        newDiv.classList.add('empty-cart')
+        cart.appendChild(newDiv)
+        hasEmptyCartRunBefore = true
+
+    }
+    
+}
+
+function addCart(){
+
+    const emptyCart = document.querySelector('#newDiv');
+    if (emptyCart){
+        cart.removeChild(emptyCart)
+    }
+    
+    /* code to add initial view of cart */
+    
+}
+
+
+
+function listCart(cartItem){
+    if (cartItem.listed == true){
+        updateItem(cartItem)
+    }else{
+        listItem(cartItem)
+    }
+}
+
+function listItem(cartItem){
+    const outerDiv = document.createElement('div')
+    outerDiv.id = `${cartItem.title}`
+    const title = document.createElement('p')
+    const detail = document.createElement('p')
+    const detailQuantity = document.createElement('span')
+    const detailUnitPrice = document.createElement('span')
+    const detailPrice = document.createElement('span')
+
+    detail.appendChild(detailQuantity)
+    detail.appendChild(detailUnitPrice)
+    detail.appendChild(detailPrice)
+
+    outerDiv.appendChild(title)
+    outerDiv.appendChild(detail)
+
+    outerDiv.classList.add('cart-entry')
+
+
+
 }
 
 function listDesserts(dessert){
@@ -108,12 +163,23 @@ function listDesserts(dessert){
         addQuantity(dessert.name)
         totalItems = sumShoppingCart(shoppingCart)
         cartSize.innerText=`(${totalItems})`
+        updatePrice(dessert.name)
+        cartEmpty = false
+        hasEmptyCartRunBefore = false
+        addCart()
     })
 
     subDiv.addEventListener("click",()=>{
         subQuantity(dessert.name)
         totalItems = sumShoppingCart(shoppingCart)
         cartSize.innerText=`(${totalItems})`
+        updatePrice(dessert.name)
+        if (totalItems == 0){
+            cartEmpty = true
+            emptyCart()
+        }else{
+            addCart(dessert.name)
+        }
     })
 
     img.addEventListener("click",()=>{
@@ -122,17 +188,30 @@ function listDesserts(dessert){
         button.textContent=''
         
         if(img.classList.contains('on-selected-or-hover') && button.classList.contains('button-hover')){
+
             button.appendChild(subDiv)
             button.appendChild(document.createTextNode(`${returnQuantity(dessert.name)}`))
             button.appendChild(addDiv)
+
         } else{
+
             button.appendChild(icon)
             button.appendChild(document.createTextNode('add to cart'))
+
         }
         
         resetQuantity(dessert.name)
         totalItems = sumShoppingCart(shoppingCart)
         cartSize.innerText=`(${totalItems})`
+
+        if(totalItems == 0){
+            cartEmpty = true
+            emptyCart()
+        }
+
+        // if (totalItems == 0 && cartEmpty == false){
+        //     emptyCart()
+        // }
     })
 
     dessertList.appendChild(item)
@@ -154,7 +233,10 @@ async function getDesserts(){
 function populateCart(dessert){
     const des ={
         item: dessert.name,
-        quantity: 0
+        quantity: 0,
+        unitPrice: dessert.price,
+        price: null,
+        listed:false
     }
     shoppingCart.push(des)
 }
@@ -205,4 +287,13 @@ function sumShoppingCart(shoppingCart){
     return total;
 }
 
+function updatePrice(name){
+    let item = shoppingCart.find((item)=>{
+        return item.item == name
+    })
+    
+    item.price = item.unitPrice * item.quantity
+    return item.price
+}
 
+console.log(shoppingCart)
